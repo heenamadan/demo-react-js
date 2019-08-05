fa2220b3a7c9baa2a06da998b4ab5477605c1a4b
Heena <heena.madan4@gmail.com>
2019-08-05 22:14:42 +0530

    raect-props-example

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// //ReactDOM.render(<App />, document.getElementById('root'));
-
-// const myelement = <input type="text" />;
-
-// ReactDOM.render(myelement, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import HomePage from './homepage'
-
-ReactDOM.render(
-    <HomePage/>,
-    document.getElementById('app')
-);
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import App from './App.js';  
+  
+  //React props
+//ReactDOM.render(<App name = "Heena!!" />, document.getElementById('app'));  
 
 
-ReactDOM.render(<App name = "JavaTpoint!!" />, document.getElementById('app'));  
-
-
-
-
-
+//using default properties
+ReactDOM.render(<App/>, document.getElementById('app'));  
