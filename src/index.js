34eea178250be291e3f2e0adbb09c99febc1cf5d
Heena <heena.madan4@gmail.com>
2019-08-06 16:26:45 +0530

    component-api-changes-with-forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';  
 import ReactDOM from 'react-dom';  
-import App from './App.js';  
+//import App from './App.js'; 
+import Lifecycle from './Lifecycle.js'; 
+
+import MyForm from './MyForm.js';//uncontrolled form
+
+import ControlledForm from './ControlledForm.js';
+
+   
   
-ReactDOM.render(<App/>, document.getElementById('app')); 
+//ReactDOM.render(<App/>, document.getElementById('app')); 
+
+//for lifecycle
+ReactDOM.render(<Lifecycle/>, document.getElementById('lifecycle')); 
+//for react forms
+ReactDOM.render(<MyForm/>, document.getElementById('myform')); 
+
+ReactDOM.render(<ControlledForm/>, document.getElementById('mycontrolledform')); 
+
+
